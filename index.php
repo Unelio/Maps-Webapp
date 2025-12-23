a8e91254841b141b925c3870d9546c7c87ddab8f
Unelio <72112686+Unelio@users.noreply.github.com>
2025-12-23 21:06:36 +0100

    Création d'un fichier functions.php

--- index.php
+++ index.php
@@ -5,47 +5,7 @@ $file = ".env";
 
 if (!file_exists($file)) die('Un fichier est manquant');
 
-function parse_env_file(string $file): array {
-  if (!is_readable($file)) return [];
-  
-  $lines = file($file, FILE_IGNORE_NEW_LINES);
-  $env = [];
-  
-  foreach ($lines as $rawLine) {
-    // Retirer BOM
-    $line = preg_replace('/^\x{FEFF}/u', '', $rawLine);
-    $line = trim($line);
-    
-    if ($line === '') continue;
-    
-    // Ignorer les commentaires en début de ligne (avec ou sans espaces)
-    $ltrim = ltrim($line);
-    if (strpos($ltrim, '#') === 0 || strpos($ltrim, ';') === 0 || strpos($ltrim, '//') === 0) continue;
-    
-    // Accepter "export KEY=VALUE"
-    if (stripos($ltrim, 'export ') === 0) {
-        $ltrim = trim(substr($ltrim, 7));
-    }
-    
-    if (strpos($ltrim, '=') === false) continue;
-    
-    list($name, $value) = explode('=', $ltrim, 2);
-    $name = trim($name);
-    $value = trim($value);
-    
-    // Si la valeur est entre guillemets (simple ou double), on la décapsule
-    if (strlen($value) >= 2 && (($value[0] === '"' && substr($value, -1) === '"') || ($value[0] === "'" && substr($value, -1) === "'"))) {
-        $value = substr($value, 1, -1);
-        $value = str_replace(['\\"', "\\'"], ['"', "'"], $value);
-    } else {
-        // Enlever commentaire inline qui vient après un espace : " valeur #comment" ou "valeur ;comment" ou "valeur //comment"
-        $value = preg_replace('/\s+(#|;|\/\/).*$/', '', $value);
-        $value = rtrim($value);
-    }
-    $env[$name] = $value;
-  }
-  return $env;
-}
+include __DIR__ . '/inc/functions.php';
 
 $array = parse_env_file(".env");
 
