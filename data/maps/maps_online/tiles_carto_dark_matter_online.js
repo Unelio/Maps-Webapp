b531daed6aa1d38d8c2a88a65f3d30b14427901e
Unelio <72112686+Unelio@users.noreply.github.com>
2026-07-29 19:27:11 +0200

    Suppression de la description

--- data/maps/maps_online/tiles_carto_dark_matter_online.js
+++ data/maps/maps_online/tiles_carto_dark_matter_online.js
@@ -2,5 +2,4 @@
 L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
   subdomains: ['a', 'b', 'c', 'd'],
   maxZoom: 20,
-  attribution: '&copy; OpenStreetMap contributors &copy; CARTO'
 }).addTo(map);
--- data/maps/maps_online/tiles_carto_voyager_online.js
+++ data/maps/maps_online/tiles_carto_voyager_online.js
@@ -2,5 +2,4 @@
 L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
   subdomains: ['a', 'b', 'c', 'd'],
   maxZoom: 20,
-  attribution: '&copy; OpenStreetMap contributors &copy; CARTO'
 }).addTo(map);
